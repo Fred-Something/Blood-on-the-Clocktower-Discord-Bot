b0f31aa02ac0128bd812a6256004e7a81962bda2
adamantii <adam@sallome.com>
2024-04-15 00:33:11 +0400

    Fix deploy-commands not working on MacOS

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,11 +6,11 @@ const path = require('node:path');
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const commandFolders = fs.readdirSync(foldersPath, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
+	const commandsPath = path.join(foldersPath, folder.name);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
